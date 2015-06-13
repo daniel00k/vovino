0aaba9fcfcb91cc19799571c8cafc4a9d2556374
Ricardo Andrés Bello <ricardoaandres@me.com>
2015-06-13 13:16:27 -0300

    Add third party libraries to static path

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public/bower_components'));
 
 app.use('/', routes);
 
