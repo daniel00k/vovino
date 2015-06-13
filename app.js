dec5e36b4330f995d0de48682cee430bb74b7119
daniel aguilar <soulreborn2@gmail.com>
2015-06-13 11:44:18 -0400

    add token helper

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes       = require('./routes/index');
 
-var app = express();
+var redis        = require("redis").createClient();
+var io           = require("socket.io").listen(process.env.PORT || 3001);
+
+var helpers      = require('./utils/helpers').tokenHelpers;
+var app          = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +26,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+console.log(helpers.generatePartyId());
+redis.subscribe("alert_created");
+
+io.on("connection", function(socket){
+ console.log("connected socket")
+ socket.on("disconnect", function(){
+ console.log("client disconnected")
+ socket.disconnect();
+ });
+});
+
+redis.on("message", function(channel, message){
+    console.log(message);
+    io.sockets.emit(channel, JSON.parse(message));
+    console.log("channel "+ channel);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
