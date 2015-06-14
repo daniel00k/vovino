2e40af159078a85d9502956666c48988066c2a6f
Ricardo Andrés Bello <ricardoaandres@me.com>
2015-06-14 00:54:00 -0300

    Remove ember and reimplement EVERYTHING from scratch

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,48 +1,54 @@
-var App = Ember.Application.create({
-  LOG_TRANSITIONS: true
-});
++function () {
+  'use strict';
 
-// Routes
-App.Router.map(function () {
-  this.route('login');
-  this.route('join');
-});
+  var App = {
+    onClick: function (dest, callback) {
+      document.querySelector('[data-href="' + dest + '"]').addEventListener('click', function (e) {
+        callback(e);
+      });
+    },
 
-App.ApplicationRoute = Ember.Route.extend({
-  renderTemplate: function() {
-    var playlistSelected = false,
-        playlistID = '';
+    render: function (templateName, options, callback) {
+      var indexSource = document.getElementById(templateName).innerHTML,
+          template = Handlebars.compile(indexSource);
 
-    if (playlistSelected) {
-      this.render('with-playlist');
-    } else {
-      this.render('no-playlist');
+      callback = function () {};
+
+      if (typeof options === 'function') {
+        callback = options;
+        options = {};
+      }
+
+      document.getElementsByTagName('main')[0].innerHTML = template(options);
+
+      callback();
     }
-  }
-});
+  };
 
-App.JoinRoute = Ember.Route.extend({
-  model: function () {
-    return App.Playlist.create();
-  },
+  document.addEventListener('DOMContentLoaded', function () {
+    App.render('index');
 
-  setupController: function (controller, model) {
-    controller.set('model', model);
-  }
-});
-
-// Models
-App.Playlist = Ember.Object.extend({
-  id: ''
-});
-
-// Controllers
-App.JoinController = Ember.Controller.extend({
-  submitAction: function () {
-    var playlistID = this.get('model').get('id');
-
-    $.getJSON('/join/' + playlistID, function (data) {
-      debugger
+    App.onClick('join', function (e) {
+      App.render('join');
     });
-  }
-});
+
+    App.onClick('create', function (e) {
+      App.render('create', function () {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', '/parties', true);
+
+        xhr.onload = function (response) {
+          var data = JSON.parse(response.target.response),
+              modal = document.getElementById('new-playlist-modal');
+
+          modal.querySelector('.code').innerText = data.party_id.toUpperCase();
+
+          $(modal).modal('show');
+        };
+
+        xhr.send();
+      });
+    });
+  });
+}();
---
+++ public/javascripts/partials.js
@@ -0,0 +1,4 @@
++function () {
+  Handlebars.registerPartial('header', document.getElementById('header').innerHTML);
+  Handlebars.registerPartial('footer', document.getElementById('footer').innerHTML);
+}();
